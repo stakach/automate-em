09f74e3ddce5a80ebd08d9faf9026b80428203c1
Stephen von Takach <steve@advancedcontrol.com.au>
2013-07-03 13:39:03 +1000

    function not hash

--- lib/models/control_system.rb
+++ lib/models/control_system.rb
@@ -1,7 +1,7 @@
 class ControlSystem < ActiveRecord::Base
-	has_many :devices, -> {order: 'priority ASC'},	:class_name => "ControllerDevice",	:dependent => :destroy
-	has_many :logics, -> {order: 'priority ASC'},	:class_name => "ControllerLogic",	:dependent => :destroy
-	has_many :services, -> {order: 'priority ASC'},	:class_name => "ControllerHttpService",	:dependent => :destroy
+	has_many :devices, -> {order 'priority ASC'},	:class_name => "ControllerDevice",	:dependent => :destroy
+	has_many :logics, -> {order 'priority ASC'},	:class_name => "ControllerLogic",	:dependent => :destroy
+	has_many :services, -> {order 'priority ASC'},	:class_name => "ControllerHttpService",	:dependent => :destroy
 	
 	has_many :controller_zones,		:dependent => :destroy
 	has_many :zones,				:through => :controller_zones
